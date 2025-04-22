1ad2a499c56198e5f5c47a4aecc668d29b15e842
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2025-04-22 04:04:11 +0300

    Update RabbitMqConfig.cs

--- src/AuthService.Models.Dto/Configurations/RabbitMqConfig.cs
+++ src/AuthService.Models.Dto/Configurations/RabbitMqConfig.cs
@@ -1,6 +1,7 @@
 ﻿using UniversityHelper.Core.BrokerSupport.Attributes;
 using UniversityHelper.Core.BrokerSupport.Configurations;
 using UniversityHelper.Models.Broker.Requests.User;
+using UniversityHelper.Core.BrokerSupport.Middlewares.Token;
 
 namespace UniversityHelper.AuthService.Models.Dto.Configurations;
 
@@ -10,4 +11,7 @@ public class RabbitMqConfig : BaseRabbitMqConfig
   public string GetUserCredentialsEndpoint { get; set; }
 
   public string GetTokenEndpoint { get; set; }
+
+  [AutoInjectRequest(typeof(ICheckTokenRequest))]
+  public string ValidateTokenEndpoint { get; set; }
 }
