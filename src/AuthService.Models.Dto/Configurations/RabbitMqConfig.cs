bf499312cc23c434bbb6937217e2a338a6561fa5
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2025-04-22 04:07:37 +0300

    Revert "Update RabbitMqConfig.cs"
    This reverts commit 1ad2a499c56198e5f5c47a4aecc668d29b15e842.

--- src/AuthService.Models.Dto/Configurations/RabbitMqConfig.cs
+++ src/AuthService.Models.Dto/Configurations/RabbitMqConfig.cs
@@ -1,7 +1,6 @@
 ﻿using UniversityHelper.Core.BrokerSupport.Attributes;
 using UniversityHelper.Core.BrokerSupport.Configurations;
 using UniversityHelper.Models.Broker.Requests.User;
-using UniversityHelper.Core.BrokerSupport.Middlewares.Token;
 
 namespace UniversityHelper.AuthService.Models.Dto.Configurations;
 
@@ -11,7 +10,4 @@ public class RabbitMqConfig : BaseRabbitMqConfig
   public string GetUserCredentialsEndpoint { get; set; }
 
   public string GetTokenEndpoint { get; set; }
-
-  [AutoInjectRequest(typeof(ICheckTokenRequest))]
-  public string ValidateTokenEndpoint { get; set; }
 }
